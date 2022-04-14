02590df221f4bc6212b84112d8f86570dfa1ba15
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-14 20:12:34 +0800

    commit

--- TPC-DS/create.sh
+++ TPC-DS/create.sh
@@ -1,1 +1,10 @@
+clickhouse-client --query="DROP TABLE IF EXISTS call_center"
+clickhouse-client --query="DROP TABLE IF EXISTS catalog_sales"
+clickhouse-client --query="DROP TABLE IF EXISTS customer"
+clickhouse-client --query="DROP TABLE IF EXISTS date_dim"
+clickhouse-client --query="DROP TABLE IF EXISTS household_demographics"
+clickhouse-client --query="DROP TABLE IF EXISTS item"
+clickhouse-client --query="DROP TABLE IF EXISTS store_sales"
+clickhouse-client --query="DROP TABLE IF EXISTS store"
+clickhouse-client --query="DROP TABLE IF EXISTS web_sales"
 for sql in /home/jianglind/ck-dataset/TPC-DS/create_*.sql; do cat $sql | clickhouse-client; done
