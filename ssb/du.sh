1bc2fe0d19e6159d69253c8f205ec99d0cc36370
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-14 14:57:43 +0800

    commit

--- ssb/du.sh
+++ ssb/du.sh
@@ -1,7 +1,7 @@
 echo "---------------ck du----------------" >> log/result.log
 du -sh /mnt/disk1/ck1/ >> log/result.log
 echo "--------------filer du--------------" >> log/result.log
-du -sh /mnt/disk1/seaweed/data/buckets/tencentBucket/djl/ >> log/result.log
+du -sh /mnt/seaweed/data/buckets/tencentBucket/djl/ >> log/result.log
 echo "---------------s3 du----------------" >> log/result.log
 du -sh /mnt/s3/djl-data/ >> log/result.log
 echo "------------query result------------" >> log/result.log
