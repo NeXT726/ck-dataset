c942f627e3669d3ed6ec1e8af18896416c3832ab
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-14 19:52:08 +0800

    commit

--- TPC-DS/data-gen.sh
+++ TPC-DS/data-gen.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 mkdir /mnt/disk1/data/tpc-ds/data-$1
-./dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 1 &
-./dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 2 &
-./dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 3 &
-./dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 4 &
-./dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 5 &
-./dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 6 &
-./dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 7 &
-./dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 8 &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 1 &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 2 &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 3 &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 4 &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 5 &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 6 &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 7 &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 8 &
