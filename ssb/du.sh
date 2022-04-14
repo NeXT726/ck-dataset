26c41ce4749051d763b483eaa4ece7aa013e886e
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-14 14:56:39 +0800

    commit

--- ssb/du.sh
+++ ssb/du.sh
@@ -1,10 +1,10 @@
-echo "---------------ck du----------------" >> result.log
-du -sh /mnt/disk1/ck1/ >> result.log
-echo "--------------filer du--------------" >> result.log
-du -sh /mnt/disk1/seaweed/data/buckets/tencentBucket/djl/ >> result.log
-echo "---------------s3 du----------------" >> result.log
-du -sh /mnt/s3/djl-data/ >> result.log
-echo "------------query result------------" >> result.log
-sh ./sh/run_query.sh single >> result.log 2>query-single-1.log
-sh ./sh/run_query.sh single >> result.log 2>query-single-2.log
-sh ./sh/run_query.sh single >> result.log 2>query-single-3.log
+echo "---------------ck du----------------" >> log/result.log
+du -sh /mnt/disk1/ck1/ >> log/result.log
+echo "--------------filer du--------------" >> log/result.log
+du -sh /mnt/disk1/seaweed/data/buckets/tencentBucket/djl/ >> log/result.log
+echo "---------------s3 du----------------" >> log/result.log
+du -sh /mnt/s3/djl-data/ >> log/result.log
+echo "------------query result------------" >> log/result.log
+sh ./sh/run_query.sh single >> log/result.log 2>log/query-single-1.log
+sh ./sh/run_query.sh single >> log/result.log 2>log/query-single-2.log
+sh ./sh/run_query.sh single >> log/result.log 2>log/query-single-3.log
--- ssb/run_test.sh
+++ ssb/run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh ./sh/run_load_flat_single.sh $1 2>load_single.log
-sh ./sh/run_query.sh $2 2>query-single-1.log
-sh ./sh/run_query.sh $2 2>query-single-2.log
-sh ./sh/run_query.sh $2 2>query-single-3.log
+sh ./sh/run_load_flat_single.sh $1 2>log/load_single.log
+sh ./sh/run_query.sh $2 2>log/query-single-1.log
+sh ./sh/run_query.sh $2 2>log/query-single-2.log
+sh ./sh/run_query.sh $2 2>log/query-single-3.log
