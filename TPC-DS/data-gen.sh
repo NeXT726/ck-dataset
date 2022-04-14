a756d71dc88f25a30618bb771aab6201225e21ec
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-14 19:54:02 +0800

    commit

--- TPC-DS/data-gen.sh
+++ TPC-DS/data-gen.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 mkdir /mnt/disk1/data/tpc-ds/data-$1
-/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 1 &
-/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 2 &
-/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 3 &
-/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 4 &
-/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 5 &
-/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 6 &
-/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 7 &
-/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 8 &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 1 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 2 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 3 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 4 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 5 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 6 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 7 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -parallel 8 -child 8 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx &
