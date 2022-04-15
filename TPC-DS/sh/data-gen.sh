aebc53121c78c43f56d40532170e4213775a00eb
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-15 11:33:00 +0800

    commit

--- TPC-DS/sh/data-gen.sh
+++ TPC-DS/sh/data-gen.sh
@@ -2,4 +2,4 @@
 
 rm -rf /mnt/disk1/data/tpc-ds/data-$1/
 mkdir /mnt/disk1/data/tpc-ds/data-$1
-/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx &
+/mnt/disk1/data/tpc-ds/dsdgen -scale $1 -dir /mnt/disk1/data/tpc-ds/data-$1 -DISTRIBUTIONS /home/jianglind/tpcds-kit/tools/tpcds.idx
