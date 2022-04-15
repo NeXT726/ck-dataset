c165d984488030dbdb5c8ed77f60098662f97bb0
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-15 14:55:05 +0800

    commit

--- TPC-DS/sh/create.sh
+++ TPC-DS/sh/create.sh
@@ -6,3 +6,6 @@ do
     clickhouse-client --query="DROP TABLE IF EXISTS $line" &&
     cat ../create_table/$line.sql | clickhouse-client
 done < ../create_table/table.lst
+
+echo "databases:"
+clickhouse-client --query="show databases"
