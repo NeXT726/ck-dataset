25e565b1bb7eea5b8fc7515b828c0a28483a40ae
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-15 18:25:34 +0800

    commit

--- TPC-DS/sh/sql-gen.sh
+++ TPC-DS/sh/sql-gen.sh
@@ -1,9 +1,10 @@
 
 for i in {1..99}
 do /home/jianglind/tpcds-kit/tools/dsqgen \
-    -input /home/jianglind/tpcds-kit/query_templates/templates.lst \
-    -directory /home/jianglind/tpcds-kit/query_templates \
-    -dialect sqlserver \
-    -output_dir /mnt/disk1/data/tpc-ds/sql \
-    -TEMPLATE "query${i}.tpl"
+    -INPUT /home/jianglind/tpcds-kit/query_templates/templates.lst \
+    -DIRECTORY /home/jianglind/tpcds-kit/query_templates \
+    -DIALECT sqlserver \
+    -OUTPUT_DIR /mnt/disk1/data/tpc-ds/sql \
+    -TEMPLATE "query${i}.tpl" \
+    -SCALE 10
 done
