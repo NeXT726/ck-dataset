5fdb1b1e3d17d9f87dc52e255f51b918224f1268
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-15 18:02:44 +0800

    commit

--- TPC-DS/sh/query.sh
+++ TPC-DS/sh/query.sh
@@ -1,3 +1,3 @@
 for sql in ../query/$1/*.sql; 
-    do echo -e "\nquery $sql" && echo -n "$sql">&2 && time(cat $sql | clickhouse-client) && echo ""; 
+    do echo -e "query $sql" && echo -n "$sql">&2 && time(cat $sql | clickhouse-client) && echo "" && echo "">&2; 
 done 1>../log/$1-query.log 2>../log/$1-time.log
