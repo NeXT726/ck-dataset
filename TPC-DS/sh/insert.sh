c9c597c03e5ce0fca3be20f578695fd20f67b2b8
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-15 16:49:35 +0800

    commit

--- TPC-DS/sh/insert.sh
+++ TPC-DS/sh/insert.sh
@@ -3,5 +3,5 @@
 while read line
 do
     echo "insert $line"
-    cat /mnt/disk1/data/tpc-ds/data-10/$line.csv | clickhouse-client --format_csv_delimiter="|" --query="INSERT INTO catalog_sales FORMAT CSV" --max_partitions_per_insert_block=0
+    cat /mnt/disk1/data/tpc-ds/data-10/$line.csv | clickhouse-client --format_csv_delimiter="|" --query="INSERT INTO $line FORMAT CSV" --max_partitions_per_insert_block=0
 done < ../create_table/table.lst
