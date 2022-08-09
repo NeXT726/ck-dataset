d224e5f323df811b7e214a01934dbb806177d964
ubuntu <ubuntu@localhost.localdomain>
2022-08-09 18:57:37 +0800

    commit

--- BigData/sh/query.sh
+++ BigData/sh/query.sh
@@ -1,4 +1,6 @@
 for sql in ../sql/query/*.sql; 
-    do clickhouse-client --queries-file=$sql; 
+    do 
+	    echo $sql
+	    clickhouse-client --time --format=Null --queries-file=$sql 2>&1
 done
 # done 1>../log/result.log
